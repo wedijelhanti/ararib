6bd05b3bce7160a3c4f6b9fbcbdc10a86aea2df6
Biniam <wedijelhanti@googlemail.com>
2023-05-29 16:03:47 +0100

    This is first change of the wbsite

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        
+
+          <div className="flex flex-col justify-center items-center bg-cover bg-clip-text bg-center uppercase text-transparent text-4xl sm:text-7xl font-extrabold tracking-wide antialiased" style={{ backgroundImage: "url(https://media.giphy.com/media/xTiTniuHdUjpOlNo1q/source.gif)" }}>
+        Welcome to Ararib.co.uk
+
+      </div>
+
+    </main>
+  )
+}
